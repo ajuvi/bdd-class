a0f051e698211b97296ac1a0a76a372df0aa0408
ajuvi <102821845+ajuvi@users.noreply.github.com>
2023-05-03 08:11:37 +0200

    artur

--- hospital2.sql
+++ hospital2.sql
@@ -4,7 +4,7 @@
 ------------------
 
 ---
--- Eliminació de les taules
+-- EliminaciÃ³ de les taules
 ---
 DROP TABLE dept CASCADE CONSTRAINTS;   
 DROP TABLE doctor CASCADE CONSTRAINTS;
@@ -14,10 +14,11 @@ DROP TABLE malalt CASCADE CONSTRAINTS;
 DROP TABLE empl CASCADE CONSTRAINTS;
 DROP TABLE plantilla CASCADE CONSTRAINTS;
 DROP TABLE horari CASCADE CONSTRAINTS;
+DROP TABLE visita CASCADE CONSTRAINTS;
 
 drop sequence horari_seq;
 
--- Creació de taules 
+-- CreaciÃ³ de taules 
 
 CREATE TABLE dept
  (id  NUMBER(11) primary key,
@@ -35,16 +36,18 @@ CREATE TABLE empl
   dept_id   NUMBER(11),
   
   CONSTRAINT fk_empl_dept_id
-    foreign key (dept_id)
-    references dept(id)
+    FOREIGN KEY (dept_id)
+    REFERENCES dept(id)
   );
 
 CREATE TABLE hospital
- (id        NUMBER(11) primary key,
+ (id        NUMBER(11),
   nom       VARCHAR2(12),
   adreca    VARCHAR2(20),
   telefon   VARCHAR2(8),
-  nllits    NUMBER(11)
+  nllits    NUMBER(11),
+  
+  CONSTRAINT pk_hospital PRIMARY KEY(id)
   );
 
 CREATE TABLE sala
@@ -55,27 +58,27 @@ CREATE TABLE sala
   
   CONSTRAINT pk_sala PRIMARY KEY(id),
   CONSTRAINT fk_sala_hospital
-    foreign key (hospital_id)
-    references hospital(id)
+    FOREIGN KEY (hospital_id)
+    REFERENCES hospital(id)
   );
 
 CREATE TABLE plantilla
  ( 
-    id                   NUMBER(11),
-    hospital_id          NUMBER(11),
-    sala_id              NUMBER(11),
-    nom                  VARCHAR2(16),
-    funcio               VARCHAR2(10),
-    torn                 VARCHAR2(1),
-    salari               NUMBER(11,2),
+    id              NUMBER(11),
+    hospital_id     NUMBER(11),
+    sala_id         NUMBER(11),
+    nom             VARCHAR2(16),
+    funcio          VARCHAR2(10),
+    torn            VARCHAR2(1),
+    salari          NUMBER(11,2),
    
     CONSTRAINT pk_plantilla PRIMARY KEY(id),
     CONSTRAINT fk_plantilla_hospital
-        foreign key (hospital_id)
-        references hospital(id),
-   CONSTRAINT fk_plantilla_sala
-        foreign key (sala_id)
-        references sala(id)    
+        FOREIGN KEY (hospital_id)
+        REFERENCES hospital(id),
+    CONSTRAINT fk_plantilla_sala
+        FOREIGN KEY (sala_id)
+        REFERENCES sala(id)    
  );
 
 CREATE TABLE doctor
@@ -100,109 +103,131 @@ CREATE TABLE horari (
     REFERENCES doctor(id)	
 );
 
+CREATE SEQUENCE visita_seq
+    START WITH 1
+    INCREMENT BY 1;
+
+
 CREATE SEQUENCE horari_seq
     START WITH 1
     INCREMENT BY 1;
 
 CREATE TABLE malalt
- (id    NUMBER(11) primary key,
-  nom    VARCHAR2(12),
-  adreca VARCHAR2(20),
-  dnaixa DATE,
-  sexe   VARCHAR2(1),
-  nss    NUMBER(9),
-  hospital_id NUMBER(11),
-  doctor_id NUMBER(11),
+ (id            NUMBER(11),
+  nom           VARCHAR2(12),
+  adreca        VARCHAR2(20),
+  dnaixa        DATE,
+  sexe          VARCHAR2(1),
+  nss           NUMBER(9),
+  hospital_id   NUMBER(11),
+  doctor_id     NUMBER(11),
   
+  CONSTRAINT pk_malalt PRIMARY KEY(id),  
   CONSTRAINT fk_malalt_hospital
-    foreign key (hospital_id)
-    references hospital(id),
+    FOREIGN KEY (hospital_id)
+    REFERENCES hospital(id),
   CONSTRAINT fk_malalt_doctor
-    foreign key (doctor_id)
-    references doctor(id)
+    FOREIGN KEY (doctor_id)
+    REFERENCES doctor(id)
   );
 
--- Inserció de dades
+CREATE TABLE visita(
+    id              NUMBER(11),
+    sala_id         NUMBER(2) NOT NULL,
+    doctor_id       NUMBER(3) NOT NULL,
+    malalt_id       NUMBER(5) NOT NULL,
+    data            DATE  NOT NULL,
+    data_creat      DATE DEFAULT SYSDATE,
+    informe         VARCHAR2(200) DEFAULT '',
+    estat           NUMBER(1) DEFAULT 0,
+    
+    CONSTRAINT pk_visita PRIMARY KEY(id),  
+    CONSTRAINT fk_visita_sala FOREIGN KEY (sala_id) REFERENCES sala(id),
+    CONSTRAINT fk_visita_doctor FOREIGN KEY (doctor_id) REFERENCES doctor(id),
+    CONSTRAINT fk_visita_malalt FOREIGN KEY (malalt_id) REFERENCES malalt(id)
+);
 
-insert into dept values (10,'Comptabilitat','Sevilla');
-insert into dept values (20,'Investigacio','Madrid');
-insert into dept values (30,'Vendes','Barcelona');
-insert into dept values (40,'Produccio','Bilbao');
+-- InserciÃ³ de dades
 
-insert into empl values
-(7369,'SANCHEZ','EMPLEAT',7902,to_DATE('17-12-1980','dd-mm-yyyy'),104000,null,20);
-insert into empl values
-(7499,'ARROYO','VENEDOR',7698,to_DATE('22-02-1981','dd-mm-yyyy'),208000,39000,30);
-insert into empl values
-(7521,'SALA','VENEDOR',698,to_DATE('22-02-1981','dd-mm-yyyy'),162500,65000,30);
-insert into empl values
-(7566,'JIMENEZ','DIRECTOR',7839,to_DATE('02-04-1981','dd-mm-yyyy'),386750,null,20);
-insert into empl values
-(7654,'MARTIN','VENEDOR',7698,to_DATE('28-09-1981','dd-mm-yyyy'),182000,182000,30);
-insert into empl values
-(7698,'NEGRO','DIRECTOR',7839,to_DATE('01-05-1981','dd-mm-yyyy'),370500,null,30);
-insert into empl values
-(7782,'CEREZO','DIRECTOR',7839,to_DATE('09-06-1981','dd-mm-yyyy'),318500,null,10);
-insert into empl values
-(7788,'GIL','ANALISTA',7566,to_DATE('30-03-1987','dd-mm-yyyy'),390000,null,20);
-insert into empl values
-(7839,'REY','PRESIDENT',null,to_DATE('17-11-1981','dd-mm-yyyy'),650000,null,10);
-insert into empl values
-(7844,'TOVAR','VENEDOR',7698,to_DATE('08-09-1981','dd-mm-yyyy'),195000,0,30);
-insert into empl values
-(7876,'ALONSO','EMPLEAT',7788,to_DATE('03-05-1987','dd-mm-yyyy'),143000,null,20);
-insert into empl values
-(7900,'JIMENO','EMPLEAT',7698,to_DATE('03-12-1981','dd-mm-yyyy'),123500,null,30);
-insert into empl values
-(7902,'FERNANDEZ','ANALISTA',7566,to_DATE('03-12-1981','dd-mm-yyyy'),390000,null,20);
-insert into empl values
-(7934,'MUNOZ','EMPLEAT',7782,to_DATE('23-01-1982','dd-mm-yyyy'),169000,null,10);
+INSERT INTO dept VALUES (10,'Comptabilitat','Sevilla');
+INSERT INTO dept VALUES (20,'Investigacio','Madrid');
+INSERT INTO dept VALUES (30,'Vendes','Barcelona');
+INSERT INTO dept VALUES (40,'Produccio','Bilbao');
 
-insert into hospital values (13,'Provincial','O''Donell,20','964-4264',502);
-insert into hospital values (18,'General','Atocha, s/n','595-3111',987);
-insert into hospital values (22,'La Paz','Castellana, 1000','923-5411',412);
-insert into hospital values (45,'San Carlos','Ciudad Universitaria','597-1500',845);
+INSERT INTO empl VALUES
+(7369,'SANCHEZ','EMPLEAT',7902,TO_DATE('17-12-1980','dd-mm-yyyy'),104000,null,20);
+INSERT INTO empl VALUES
+(7499,'ARROYO','VENEDOR',7698,TO_DATE('22-02-1981','dd-mm-yyyy'),208000,39000,30);
+INSERT INTO empl VALUES
+(7521,'SALA','VENEDOR',698,TO_DATE('22-02-1981','dd-mm-yyyy'),162500,65000,30);
+INSERT INTO empl VALUES
+(7566,'JIMENEZ','DIRECTOR',7839,TO_DATE('02-04-1981','dd-mm-yyyy'),386750,null,20);
+INSERT INTO empl VALUES
+(7654,'MARTIN','VENEDOR',7698,TO_DATE('28-09-1981','dd-mm-yyyy'),182000,182000,30);
+INSERT INTO empl VALUES
+(7698,'NEGRO','DIRECTOR',7839,TO_DATE('01-05-1981','dd-mm-yyyy'),370500,null,30);
+INSERT INTO empl VALUES
+(7782,'CEREZO','DIRECTOR',7839,TO_DATE('09-06-1981','dd-mm-yyyy'),318500,null,10);
+INSERT INTO empl VALUES
+(7788,'GIL','ANALISTA',7566,TO_DATE('30-03-1987','dd-mm-yyyy'),390000,null,20);
+INSERT INTO empl VALUES
+(7839,'REY','PRESIDENT',null,TO_DATE('17-11-1981','dd-mm-yyyy'),650000,null,10);
+INSERT INTO empl VALUES
+(7844,'TOVAR','VENEDOR',7698,TO_DATE('08-09-1981','dd-mm-yyyy'),195000,0,30);
+INSERT INTO empl VALUES
+(7876,'ALONSO','EMPLEAT',7788,TO_DATE('03-05-1987','dd-mm-yyyy'),143000,null,20);
+INSERT INTO empl VALUES
+(7900,'JIMENO','EMPLEAT',7698,TO_DATE('03-12-1981','dd-mm-yyyy'),123500,null,30);
+INSERT INTO empl VALUES
+(7902,'FERNANDEZ','ANALISTA',7566,TO_DATE('03-12-1981','dd-mm-yyyy'),390000,null,20);
+INSERT INTO empl VALUES
+(7934,'MUNOZ','EMPLEAT',7782,TO_DATE('23-01-1982','dd-mm-yyyy'),169000,null,10);
 
-insert into sala values (1,13,'Cures Intensives',21);
-insert into sala values (2,13,'Psiquiatric',67);
-insert into sala values (3,18,'Cures Intensives',10);
-insert into sala values (4,18,'Cardiologia',53);
-insert into sala values (5,22,'Recuperacio',10);
-insert into sala values (6,22,'Psiquiatric',118);
-insert into sala values (7,22,'Maternitat',34);
-insert into sala values (8,45,'Cardiologia',55);
-insert into sala values (9,45,'Recuperacio',13);
-insert into sala values (10,45,'Maternitat',2);
+INSERT INTO hospital VALUES (13,'Provincial','O''Donell,20','964-4264',502);
+INSERT INTO hospital VALUES (18,'General','Atocha, s/n','595-3111',987);
+INSERT INTO hospital VALUES (22,'La Paz','Castellana, 1000','923-5411',412);
+INSERT INTO hospital VALUES (45,'San Carlos','Ciudad Universitaria','597-1500',845);
 
-insert into plantilla values 
+INSERT INTO sala VALUES (1,13,'Cures Intensives',21);
+INSERT INTO sala VALUES (2,13,'Psiquiatric',67);
+INSERT INTO sala VALUES (3,18,'Cures Intensives',10);
+INSERT INTO sala VALUES (4,18,'Cardiologia',53);
+INSERT INTO sala VALUES (5,22,'Recuperacio',10);
+INSERT INTO sala VALUES (6,22,'Psiquiatric',118);
+INSERT INTO sala VALUES (7,22,'Maternitat',34);
+INSERT INTO sala VALUES (8,45,'Cardiologia',55);
+INSERT INTO sala VALUES (9,45,'Recuperacio',13);
+INSERT INTO sala VALUES (10,45,'Maternitat',2);
+
+INSERT INTO plantilla VALUES 
 (3754,13,1,'Diaz B.','Infermera','T',226200);
-insert into plantilla values 
+INSERT INTO plantilla VALUES 
 (3106,13,2,'Hernandez J.','Infermer','T',275000);
-insert into plantilla values 
+INSERT INTO plantilla VALUES 
 (6357,18,3,'Karplus W.','Intern','T',337900);
-insert into plantilla values 
+INSERT INTO plantilla VALUES 
 (1009,22,5,'Higueras D.','Infermera','T',200500);
-insert into plantilla values 
+INSERT INTO plantilla VALUES 
 (8422,22,5,'Bocina G.','Infermer','M',183800);
-insert into plantilla values 
+INSERT INTO plantilla VALUES 
 (9901,22,6,'Nunez C.','Intern','M',221000);
-insert into plantilla values 
+INSERT INTO plantilla VALUES 
 (6065,22,7,'Rivera G.','Infermera','N',162600);
-insert into plantilla values 
+INSERT INTO plantilla VALUES 
 (7379,22,7,'Carlos R.','Infermera','T',221900);
-insert into plantilla values 
+INSERT INTO plantilla VALUES 
 (1280,45,8,'Amigo R.','Intern','N',221000);
-insert into plantilla values 
+INSERT INTO plantilla VALUES 
 (8526,45,9,'Frank H.','Infermera','T',252200);
    
-insert into doctor values (435,13,'Lopez A', 'Cardiologia');
-insert into doctor values (585,18,'Miller G.', 'Ginecologia');
-insert into doctor values (982,18,'Cajal R.', 'Cardiologia');
-insert into doctor values (453,22,'Galo D.', 'Pediatria');
-insert into doctor values (398,22,'Best D.', 'Urologia');
-insert into doctor values (386,22,'Cabeza D.', 'Psiquiatria');
-insert into doctor values (607,45,'Nino P.', 'Pediatria');
-insert into doctor values (522,45,'Adams C.', 'Neurologia');
+INSERT INTO doctor VALUES (435,13,'Lopez A', 'Cardiologia');
+INSERT INTO doctor VALUES (585,18,'Miller G.', 'Ginecologia');
+INSERT INTO doctor VALUES (982,18,'Cajal R.', 'Cardiologia');
+INSERT INTO doctor VALUES (453,22,'Galo D.', 'Pediatria');
+INSERT INTO doctor VALUES (398,22,'Best D.', 'Urologia');
+INSERT INTO doctor VALUES (386,22,'Cabeza D.', 'Psiquiatria');
+INSERT INTO doctor VALUES (607,45,'Nino P.', 'Pediatria');
+INSERT INTO doctor VALUES (522,45,'Adams C.', 'Neurologia');
 
 -- horari general doctor 435
 INSERT INTO horari VALUES (horari_seq.NEXTVAL, 435, 1, 8, 14);
@@ -273,25 +298,32 @@ INSERT INTO horari VALUES (horari_seq.NEXTVAL, 522, 3, 16, 19);
 INSERT INTO horari VALUES (horari_seq.NEXTVAL, 522, 4, 8, 15);
 INSERT INTO horari VALUES (horari_seq.NEXTVAL, 522, 4, 16, 19);
 
-insert into malalt values 
-(10995,'Laguia M.','Goya 20',to_DATE('16-05-1956','dd-mm-yyyy'),'M',280862482,13,435);
-insert into malalt values 
-(18004,'Serrano V.','Alcala 12',to_DATE('21-05-1960','dd-mm-yyyy'),'F',284991452,13,435);
-insert into malalt values 
-(14024,'Fernandez M.','Recoletos 20',to_DATE('23-06-1967','dd-mm-yyyy'),'F',321790059,NULL,NULL);
-insert into malalt values 
-(36658,'Domin S.','Major 71',to_DATE('01-01-1942','dd-mm-yyyy'),'M',160654471,18,982);
-insert into malalt values 
-(38702,'Neal R.','Orense 11',to_DATE('18-06-1940','dd-mm-yyyy'),'F',380010217,18,982);
-insert into malalt values
-(39217,'Cervantes M.','Peron 38',to_DATE('29-02-1952','dd-mm-yyyy'),'M',440294390,18,982);
-insert into malalt values 
-(59076,'Miller B.','Lopez de Hoyos 2',to_DATE('16-09-1945','dd-mm-yyyy'),'F',311969044,22,453);
-insert into malalt values 
-(63827,'Ruiz P.','Esquerdo 103',to_DATE('26-12-1980','dd-mm-yyyy'),'M',100973253,22,398);
-insert into malalt values 
-(64823,'Fraser A.','Soto 3',to_DATE('10-07-1980','dd-mm-yyyy'),'F',285201776,22,386);
-insert into malalt values
-(74835,'Benitez E.','Argentina 5',to_DATE('05-10-1957','dd-mm-yyyy'),'M',154811767,45,607);
+INSERT INTO malalt VALUES 
+(10995,'Laguia M.','Goya 20',TO_DATE('16-05-1956','dd-mm-yyyy'),'M',280862482,13,435);
+INSERT INTO malalt VALUES 
+(18004,'Serrano V.','Alcala 12',TO_DATE('21-05-1960','dd-mm-yyyy'),'F',284991452,13,435);
+INSERT INTO malalt VALUES 
+(14024,'Fernandez M.','Recoletos 20',TO_DATE('23-06-1967','dd-mm-yyyy'),'F',321790059,NULL,NULL);
+INSERT INTO malalt VALUES 
+(36658,'Domin S.','Major 71',TO_DATE('01-01-1942','dd-mm-yyyy'),'M',160654471,18,982);
+INSERT INTO malalt VALUES 
+(38702,'Neal R.','Orense 11',TO_DATE('18-06-1940','dd-mm-yyyy'),'F',380010217,18,982);
+INSERT INTO malalt VALUES
+(39217,'Cervantes M.','Peron 38',TO_DATE('29-02-1952','dd-mm-yyyy'),'M',440294390,18,982);
+INSERT INTO malalt VALUES 
+(59076,'Miller B.','Lopez de Hoyos 2',TO_DATE('16-09-1945','dd-mm-yyyy'),'F',311969044,22,453);
+INSERT INTO malalt VALUES 
+(63827,'Ruiz P.','Esquerdo 103',TO_DATE('26-12-1980','dd-mm-yyyy'),'M',100973253,22,398);
+INSERT INTO malalt VALUES 
+(64823,'Fraser A.','Soto 3',TO_DATE('10-07-1980','dd-mm-yyyy'),'F',285201776,22,386);
+INSERT INTO malalt VALUES
+(74835,'Benitez E.','Argentina 5',TO_DATE('05-10-1957','dd-mm-yyyy'),'M',154811767,45,607);
+
+INSERT INTO visita (id, sala_id, doctor_id, malalt_id, data)
+VALUES (visita_seq.NEXTVAL, 1, 435, 10995, TO_DATE('2023-05-04 10:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO visita (id, sala_id, doctor_id, malalt_id, data)
+VALUES (visita_seq.NEXTVAL, 1, 435, 18004, TO_DATE('2023-05-07 11:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO visita (id, sala_id, doctor_id, malalt_id, data)
+VALUES (visita_seq.NEXTVAL, 2, 435, 14024, TO_DATE('2023-05-15 12:00:00', 'YYYY-MM-DD HH24:MI:SS'));
 
 COMMIT;
