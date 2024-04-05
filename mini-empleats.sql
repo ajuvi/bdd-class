32983851f3fb76e76298803be51b8b35d571d6e9
ajuvi <102821845+ajuvi@users.noreply.github.com>
2024-04-05 10:47:10 +0200

    artur

--- mini-empleats.sql
+++ mini-empleats.sql
@@ -1,3 +1,5 @@
+SET AUTOCOMMIT OFF
+
 drop table dept cascade constraints;   
 drop table empl cascade constraints;
 
@@ -5,7 +7,7 @@ drop table empl cascade constraints;
 
 create table dept(
     id number(2),
-    nom  varchar2(14) no null,
+    nom  varchar2(14) not null,
     loc  varchar2(13),
     constraint pk_dept 
         primary key(id)
@@ -48,37 +50,37 @@ insert into dept values (50,'Testing','Girona');
 
 -- Insercci de dades a la taula EMPL
 insert into empl values
-(7839,'REY','PRESIDENT',null,to_date('17-11-1981','dd-mm-yyyy'),to_date('01-12-1960','dd-mm-yyyy'),650000,null,10);
+(7839,'REY','PRESIDENT',null,to_date('17-11-1981','dd-mm-yyyy'),to_date('01-12-1960','dd-mm-yyyy'),650000,null,10,default);
 insert into empl values
-(7566,'JIMENEZ','DIRECTOR',7839,to_date('02-04-1981','dd-mm-yyyy'),to_date('11-11-1965','dd-mm-yyyy'),386750,null,20);
+(7566,'JIMENEZ','DIRECTOR',7839,to_date('02-04-1981','dd-mm-yyyy'),to_date('11-11-1965','dd-mm-yyyy'),386750,null,20,default);
 insert into empl values
-(7902,'FERNANDEZ','ANALISTA',7566,to_date('03-12-1998','dd-mm-yyyy'),to_date('17-11-1977','dd-mm-yyyy'),390000,null,20);
+(7902,'FERNANDEZ','ANALISTA',7566,to_date('03-12-1998','dd-mm-yyyy'),to_date('17-11-1977','dd-mm-yyyy'),390000,null,20,default);
 insert into empl values
-(7369,'SANCHEZ','EMPLEAT',7902,to_date('17-12-1980','dd-mm-yyyy'),to_date('20-01-1950','dd-mm-yyyy'),104000,null,20);
+(7369,'SANCHEZ','EMPLEAT',7902,to_date('17-12-1980','dd-mm-yyyy'),to_date('20-01-1950','dd-mm-yyyy'),104000,null,20,default);
 insert into empl values
-(7698,'NEGRO','DIRECTOR',7839,to_date('01-05-1981','dd-mm-yyyy'),to_date('23-11-1955','dd-mm-yyyy'),370500,null,30);
+(7698,'NEGRO','DIRECTOR',7839,to_date('01-05-1981','dd-mm-yyyy'),to_date('23-11-1955','dd-mm-yyyy'),370500,null,30,default);
 insert into empl values
-(7782,'CEREZO','DIRECTOR',7839,to_date('09-06-1981','dd-mm-yyyy'),to_date('02-02-1967','dd-mm-yyyy'),318500,null,10);
+(7782,'CEREZO','DIRECTOR',7839,to_date('09-06-1981','dd-mm-yyyy'),to_date('02-02-1967','dd-mm-yyyy'),318500,null,10,default);
 insert into empl values
-(7499,'ARROYO','VENEDOR',7698,to_date('22-02-1981','dd-mm-yyyy'),to_date('01-01-1939','dd-mm-yyyy'),208000,39000,30);
+(7499,'ARROYO','VENEDOR',7698,to_date('22-02-1981','dd-mm-yyyy'),to_date('01-01-1939','dd-mm-yyyy'),208000,39000,30,default);
 insert into empl values
-(7521,'SALA','VENEDOR',7698,to_date('22-02-2020','dd-mm-yyyy'),to_date('20-03-1977','dd-mm-yyyy'),162500,65000,30);
+(7521,'SALA','VENEDOR',7698,to_date('22-02-2020','dd-mm-yyyy'),to_date('20-03-1977','dd-mm-yyyy'),162500,65000,30,default);
 insert into empl values
-(7654,'MARTIN','VENEDOR',7698,to_date('23-09-2015','dd-mm-yyyy'),to_date('12-09-1988','dd-mm-yyyy'),182000,182000,30);
+(7654,'MARTIN','VENEDOR',7698,to_date('23-09-2015','dd-mm-yyyy'),to_date('12-09-1988','dd-mm-yyyy'),182000,182000,30,default);
 insert into empl values
-(7788,'GIL','ANALISTA',7566,to_date('30-03-1999','dd-mm-yyyy'),to_date('17-10-1975','dd-mm-yyyy'),390000,null,20);
+(7788,'GIL','ANALISTA',7566,to_date('30-03-1999','dd-mm-yyyy'),to_date('17-10-1975','dd-mm-yyyy'),390000,null,20,default);
 insert into empl values
-(7844,'TOVAR','VENEDOR',7698,to_date('08-09-2009','dd-mm-yyyy'),to_date('18-11-1978','dd-mm-yyyy'),195000,0,30);
+(7844,'TOVAR','VENEDOR',7698,to_date('08-09-2009','dd-mm-yyyy'),to_date('18-11-1978','dd-mm-yyyy'),195000,0,30,default);
 insert into empl values
-(7876,'ALONSO','EMPLEAT',7788,to_date('03-05-2005','dd-mm-yyyy'),to_date('09-09-1981','dd-mm-yyyy'),143000,null,20);
+(7876,'ALONSO','EMPLEAT',7788,to_date('03-05-2005','dd-mm-yyyy'),to_date('09-09-1981','dd-mm-yyyy'),143000,null,20,default);
 insert into empl values
-(7900,'JIMENO','EMPLEAT',7698,to_date('03-12-2004','dd-mm-yyyy'),to_date('20-07-1979','dd-mm-yyyy'),123500,null,30);
+(7900,'JIMENO','EMPLEAT',7698,to_date('03-12-2004','dd-mm-yyyy'),to_date('20-07-1979','dd-mm-yyyy'),123500,null,30,default);
 insert into empl values
-(7934,'MUNOZ','EMPLEAT',7782,to_date('23-01-2016','dd-mm-yyyy'),to_date('11-12-1988','dd-mm-yyyy'),169000,null,10);
+(7934,'MUNOZ','EMPLEAT',7782,to_date('23-01-2016','dd-mm-yyyy'),to_date('11-12-1988','dd-mm-yyyy'),169000,null,10,default);
 insert into empl values
-(8100,'ARNAU','EMPLEAT',7902,to_date('14-12-2023','dd-mm-yyyy'),to_date('13-04-1998','dd-mm-yyyy'),24000,null,null);
+(8100,'ARNAU','EMPLEAT',7902,to_date('14-12-2023','dd-mm-yyyy'),to_date('13-04-1998','dd-mm-yyyy'),24000,null,null,default);
 insert into empl values
-(8200,'ALBA','EMPLEAT',null,to_date('14-12-2023','dd-mm-yyyy'),to_date('11-08-1981','dd-mm-yyyy'),1000000,null,null);
+(8200,'ALBA','EMPLEAT',null,to_date('14-12-2023','dd-mm-yyyy'),to_date('11-08-1981','dd-mm-yyyy'),1000000,null,null,default);
 
 -- Confirmar els canvis
 commit;
